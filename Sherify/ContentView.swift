c21c957ca840dd9a2ae2c6c6d5fdd83db536d3f6
Omar Abou Chaer <aboushaar.omar@gmail.com>
2025-03-08 19:25:44 -0500

    Initial Commit

---
+++ Sherify/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sherify
+//
+//  Created by Omar Abou Chaer on 2025-03-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sherify/SherifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SherifyApp.swift
+//  Sherify
+//
+//  Created by Omar Abou Chaer on 2025-03-08.
+//
+
+import SwiftUI
+
+@main
+struct SherifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
